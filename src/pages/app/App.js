c98a9632be0b6249e8c0da4787d2efcb2a699405
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-11-04 21:45:29 -0300

    Criando projeto Frases de Naruto

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,55 +1,30 @@
-import { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import narutoImg from '../../images/naruto.png';
-import jutsoSound from '../../sounds/jutso.mp3';
-import { Quotes } from '../../components';
-import { getQuote } from '../../services';
 
-const audio = new Audio(jutsoSound);
+import React from "react";
+import styled from "styled-components";
+import { Quotes } from "../../components/quotes/Quotes";
 
-export function App() {
-  const isMounted = useRef(true);
-  const [quote, setQuote] = useState({
-    speaker: 'Loading speaker...',
-    quote: 'Loading Quote'
-  });
+import narutoImage from "../../images/naruto.png"
 
-  const onUpdate = async () => {
-    const resQuote = await getQuote();
-
-    if (isMounted.current) {
-      setQuote(resQuote);
-      audio.play();
-    }
-  };
-
-  useEffect(() => {
-    onUpdate();
-
-    return () => {
-      isMounted.current = false;
-    };
-  }, []);
-
-  return (
+export const App = () =>{
+  return(
     <Content>
-      <Quotes {...quote} onUpdate={onUpdate} />
-      <NarutoImg alt="Naruto holding a kunai" src={narutoImg} />
+      <Quotes quote={'ok'} speaker={'Speaker'}/>    
+      <NarutoImg src={narutoImage} alt="Naruto with kunai"/>
     </Content>
-  );
+    
+  )
 }
 
+
 const Content = styled.div`
-  height: 100vh;
-  box-sizing: border-box;
-  padding: 0 50px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-`;
+height: 100vh;
+padding: 0 50px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
 
 const NarutoImg = styled.img`
   max-width: 50vw;
   align-self: flex-end;
-`;
+`
