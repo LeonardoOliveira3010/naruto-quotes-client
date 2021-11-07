f64a7cffdc3208663f52c493df81a8e23ad43c2e
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-11-07 10:20:00 -0300

    Criando a lógica utilizando Hooks

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,20 +1,44 @@
 
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { Quotes } from "../../components/quotes/Quotes";
+import{ getQuote } from '../../services'
+import JutsuSound from '../../sounds/jutso.mp3'
 
 import narutoImage from "../../images/naruto.png"
 
+const audio = new Audio (JutsuSound)
+
 export const App = () =>{
+  const isMounted = useRef(true)
+  const [quoteState, setQuoteState] = useState({ quote: 'loading...', speaker: 'loading Speaker...' })
+
+  const onUpdate = async () =>{
+    const quote = await getQuote()
+
+    if(isMounted.current){
+      audio.play()
+      setQuoteState(quote)
+    } 
+  }
+
+  useEffect(() => {
+    onUpdate() 
+    return () => isMounted.current = false
+  }, [])
   return(
     <Content>
-      <Quotes quote={'ok'} speaker={'Speaker'}/>    
+      <Quotes 
+        quote = {quoteState.quote}
+        speaker = {quoteState.speaker}
+        onUpdate = {onUpdate}
+      />    
       <NarutoImg src={narutoImage} alt="Naruto with kunai"/>
     </Content>
     
   )
 }
-
 
 const Content = styled.div`
 height: 100vh;
