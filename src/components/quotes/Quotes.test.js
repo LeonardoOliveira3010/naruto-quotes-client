b30f480305ba6c7c3be32f4554d62b12b0f67d1d
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-11-07 10:20:39 -0300

    Criando testes TDD

--- src/components/quotes/Quotes.test.js
+++ src/components/quotes/Quotes.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Quotes } from "./Quotes";
 
 const quote = 'test quote'
@@ -8,9 +8,19 @@ test('renders received quote, speaker and a button', () =>{
   render(<Quotes quote={quote} speaker={speaker} />)
   const quoteEl = screen.getByText(quote)
   const speakerEl = screen.getByText(speaker)
-  const button = screen.getByRole('button')
+  const buttonEl = screen.getByRole('button')
 
   expect(quoteEl).toBeInTheDocument()
   expect(speakerEl).toBeInTheDocument()
-  expect(button).toBeInTheDocument()
+  expect(buttonEl).toBeInTheDocument()
+})
+
+test('calls a callback when button is pressed', () =>{
+  const callback = jest.fn() // Diz se a função foi chamada ou não
+  render(<Quotes quote={quote} speaker={speaker} onUpdate={callback} />)
+
+  const buttonEl = screen.getByRole('button')
+
+  fireEvent.click(buttonEl)
+  expect(callback).toHaveBeenCalledTimes(1)
 })
