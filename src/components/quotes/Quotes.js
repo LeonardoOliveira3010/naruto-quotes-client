bb33459c4a977a0c31349c236d7679bad8214dcf
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-11-07 10:19:08 -0300

    Ajustando os testes referente as frases do projeto

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,18 +1,20 @@
 import styled from "styled-components"
-import {string} from 'prop-types'
+import {string, func} from 'prop-types'
+import { Button } from "../button/Button"
 
-export const Quotes = ({ quote, speaker }) =>{
+export const Quotes = ({ quote, speaker, onUpdate }) =>{
   return (
     <Wrapper>
       <Quote>{quote}</Quote>
       <Speaker>{speaker}</Speaker>
-      <button>Quote no Jutsu</button>
+      <Button onclick={onUpdate}>Quote no Jutsu</Button>
     </Wrapper>
   )
 }
 Quotes.propTypes = {
   quote: string,
   speaker: string,
+  onUpdate: func
 }
 
 const Wrapper = styled.div`
